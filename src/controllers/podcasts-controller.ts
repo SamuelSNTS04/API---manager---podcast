83c5cbf5a673d8dc5f9c7c3c28dbe22dadcfdb1c
SamuelSNTS04 <malakaigamer5@gmail.com>
2025-10-15 16:12:33 -0300

    Segunda parte da API

--- src/controllers/podcasts-controller.ts
+++ src/controllers/podcasts-controller.ts
@@ -1,9 +1,18 @@
 import { IncomingMessage, ServerResponse } from 'http';
+import { serviceListEpisodes } from '../services/list-episodes-service';
+import { serviceFilterEpisodes } from '../services/filter-episodes-service';
 
 
 export const getListEpisodes = async (req: IncomingMessage, res: ServerResponse) => {
+    const content = await serviceListEpisodes();
+    
     res.writeHead(200, { "content-type": "application/json" });
-    res.end(JSON.stringify({
-        name: "samuel"
-    }));
+    res.end(JSON.stringify(content));
+}
+
+export const getFilterEpisodes = async (req: IncomingMessage, res: ServerResponse) => {
+    const content = await serviceFilterEpisodes('flow');
+
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(JSON.stringify(content));
 }
